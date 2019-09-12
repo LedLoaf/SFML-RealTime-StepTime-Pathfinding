db58db529aa8f710972d7e8d2c480031b437b3f3
Bob <4109153+LedLoaf@users.noreply.github.com>
2019-09-11 17:39:13 -0700

    Update Entity.h

--- Pathfinding_Demo/Entity.h
+++ Pathfinding_Demo/Entity.h
@@ -14,7 +14,7 @@ public:
 	/* handle events */
 	void handleInput();
 	/* basic update logic */
-	void update(float timeDelta = 0.333f);
+	void update(Level& level, float dt);
 	/* drawing the entity */
 	void draw(sf::RenderWindow* window);
 
@@ -25,29 +25,34 @@ public:
 
 	/* start the pathfinding search */
 	void updatePathfinding(Level& level, sf::Vector2f targetPosition);
+private:
 	/* show the pathfinding in real time positions */
 	void updateByRealTime(Level& level, float dt);		// level is just used for colors
 	/* show the pathfinding in step time positions */
 	void updateByStepTime(Level& level);
-
-	void turnOnAllColors();
-	void turnOffAllColors();
-	void toggleTilesNotCheckedColors();
 private:
 	std::vector<sf::Vector2f> m_targetPositionsRealTime;			// for storing the path in real-time positions
 	std::vector<sf::Vector2f> m_targetPositionsStepTime;			// for storing the path in step-time positions (one tile at a time)
 
 	float m_speed;						// basic movement speed for real-time
-	sf::RectangleShape m_sprite;		// obviously can be a sprite
-	bool m_allowDiagonals;				// allows for 8 directions during pathfinding
+	sf::RectangleShape m_sprite;				// obviously can be a sprite
 	Tile* m_startNode;					// the node to start the pathfinding
-	Tile* m_goalNode;						// the node to end the pathfinding
+	Tile* m_goalNode;					// the node to end the pathfinding
 
 	sf::Vector2f m_velocity;			// velocity for real-time pathfinding
 	sf::Vector2f m_newPosition;			// continue from last position (for real-time pathfinding)
 
 	/* too lazy for getters/setters */
 public:
+	void setAllColorsStatus(bool status);
+	void toggleTilesNotCheckedColors();
+	/* to display information for visuals*/
+	Tile* getStartNode() const;
+	Tile* getGoalNode() const;
+	bool isAllOff() const;
+	bool isRealTimeMode;
+	bool allowDiagonals;				// allows for 8 directions during pathfinding
+
 	/* variables to show colors */
 	bool showStartColor;
 	bool showGoalColor;
