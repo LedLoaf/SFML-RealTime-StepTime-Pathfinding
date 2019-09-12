cd684c5960a4c83d7b3ddbb8fa8617d3dafc0f24
Bob <4109153+LedLoaf@users.noreply.github.com>
2019-09-11 17:40:40 -0700

    Update main.cpp

--- Pathfinding_Demo/main.cpp
+++ Pathfinding_Demo/main.cpp
@@ -2,16 +2,20 @@
 #include "Entity.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
-/* can use to allow the enemies to only attack within a specific distance*/
-float distanceBetweenPoints(sf::Vector2f position1, sf::Vector2f position2) {
-	return (abs(sqrt(((position1.x - position2.x) * (position1.x - position2.x)) + ((position1.y - position2.y) * (position1.y - position2.y)))));
 
-}
+// Global Functions
+/* can use to allow the enemies to only attack within a specific distance*/
+float distanceBetweenPoints(sf::Vector2f position1, sf::Vector2f position2);
+// for visual information 
+std::string displayInfo(bool status);
+// for visual information 
+std::string displayMsg(bool status, const std::string& isTrue, const std::string& isFalse);
+
 
 /* ABOUT THIS 
  * The main functions are located in Entity.h . The function is called UpdatePathfinding.
  * I've added 2 additional functions to show Real-Time and Step-Time styles of pathfinding, which is simply just retrieving the tile position rather than by adding velocity
- * Level is pretty much irrelevant in terms of pathfinding, except for one important function restNodes(). Everything else is just for visuals
+ * Level is pretty much irrelevant in terms of pathfinding, except for one important function restNodes() and of course the Tile struct. Everything else is just for visuals
  * There are a basic bugs with switching between Real-Time and Step-Time pathfinding. This is mostly just educational for the A* algorithm.
  */
 
@@ -25,8 +29,7 @@ int main() {
 	level.load("map.map", "tiles.png");
 
 	Entity player;
-	auto ctile = level.getTile(11, 11);
-	player.setPosition(ctile->sprite.getPosition());
+	/* previous tile the player is on */
 	Tile* m_playerPreviousTile = nullptr;
 	/* the tile the player is on */
 	Tile* currentTile = level.getTile(player.getPosition());		// getTile retrieves the Tile based off the position
@@ -36,26 +39,12 @@ int main() {
 		std::cout << "Failed to load Dosis-Light.ttf\n";
 
 	sf::Text directions;
+	directions.setFillColor(sf::Color{240,128,128});		// light coral
 	directions.setFont(font);
-	directions.setCharacterSize(16);
+	directions.setCharacterSize(18);
 	directions.setStyle(sf::Text::Bold);
 
-	directions.setString(
-		"== Directions == \
-		\nWASD Keys to move view \
-		\nLeft Click tile to move \
-		\nNumber 1: Toggle diagonal pathfinding \
-		\nNumber 4: Toggle OpenList Colors \
-		\nNumber 5: Toggle FCost Colors \
-		\nNumber 6: Toggle Current Path Colors \
-		\nNumber 7: Toggle Path Colors \
-		\nNumber 8: Toggle Start Colors \
-		\nNumber 9: Toggle Goal Colors \
-		\nNumber 0: Toggle Tiles Not Checked Colors \
-		\nNumber P: Turn ON all colors \
-		\nNumber O: Turn OFF all Colors \
-		\nR key: Toggle Real-Time and Step-Time\n");
-
+	// basic clock stuff
 	sf::Clock clock;
 	sf::Time elapsedTime = sf::Time::Zero;
 	sf::Time FrameTime = sf::seconds(1.f / 15.f);		// 15 fps
@@ -72,20 +61,31 @@ int main() {
 			if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
 				window.close();
 			if(event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::Num4)
+				if (event.key.code == sf::Keyboard::Num2)
 					player.showOpenListColor = !player.showOpenListColor;
-				if (event.key.code == sf::Keyboard::Num5)
+				if (event.key.code == sf::Keyboard::Num3)
 					player.showFCostColor = !player.showFCostColor;
-				if (event.key.code == sf::Keyboard::Num6)
+				if (event.key.code == sf::Keyboard::Num4)
 					player.showCurrentPathColor = !player.showCurrentPathColor;
-				if (event.key.code == sf::Keyboard::Num7)
+				if (event.key.code == sf::Keyboard::Num5)
 					player.showPathColor = !player.showPathColor;
-				if (event.key.code == sf::Keyboard::Num8)
+				if (event.key.code == sf::Keyboard::Num6)
 					player.showStartColor = !player.showStartColor;
-				if (event.key.code == sf::Keyboard::Num9)
+				if (event.key.code == sf::Keyboard::Num7)
 					player.showGoalColor = !player.showGoalColor;
-				if (event.key.code == sf::Keyboard::Num0)
+				/* NOTE* this can't be turned on by setAllColors(true) and must be manual*/
+				if (event.key.code == sf::Keyboard::Num8)
 					player.showTilesNotCheckedColor = !player.showTilesNotCheckedColor;
+				/* turn off all color visuals */
+				if (event.key.code == sf::Keyboard::Num9)
+					player.setAllColorsStatus(false);
+				/* turn on all color visuals (NOTE* EXCEPT showTilesNotCheckedColor)*/
+				if (event.key.code == sf::Keyboard::Num0)
+					player.setAllColorsStatus(true);
+				/* toggle real-time and step-time pathfinding */
+				if (event.key.code == sf::Keyboard::R)
+					player.isRealTimeMode = !player.isRealTimeMode;
+
 			}
 		}
 
@@ -103,22 +103,6 @@ int main() {
 			view.move(10, 0);
 		}
 
-		/* turn off all color visuals */
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::O)) {
-			player.turnOffAllColors();
-		}
-		/* turn on all color visuals */
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
-			player.turnOnAllColors();
-		}
-		/* toggle real-time and step-time pathfinding */
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
-			isPathfindingRealTime = !isPathfindingRealTime;
-			if (
-				(isPathfindingRealTime) ?
-				std::cout << "Real-Time Pathfinding Enabled\n" :
-				std::cout << "Step-Time Pathfinding Enabled\n");
-		}
 
 		// execute pathfinding manually, instead of every click using space
 		/*	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
@@ -169,16 +153,7 @@ int main() {
 				}
 			}		
 
-			/* if real-time is true, use the real-time function for the positions*/
-			if (isPathfindingRealTime)
-			{
-				player.updateByRealTime(level, FrameTime.asSeconds());
-			}
-			/* if real-time is false, use the step-time function for the positions*/
-			else
-			{
-				player.updateByStepTime(level);
-			}
+			player.update(level, FrameTime.asSeconds());
 		}
 
 
@@ -192,9 +167,53 @@ int main() {
 		/* standard draw method to reveal entity sprite (rectangle)*/
 		player.draw(&window);
 		window.setView(window.getDefaultView());
+
+
+		/* some easy visuals to relay information */
+		if (currentTile)
+		{
+			std::string str =
+				"===\t\tDirections\t\t==="
+				"\nWASD Move View: position(" + std::to_string((int)player.getPosition().x) + ", " + std::to_string((int)player.getPosition().y) + ")" +
+				"\nLeft Click: Moving to Tile[" + std::to_string(currentTile->columnIndex) + "][" + std::to_string(currentTile->rowIndex) + "]" +
+				"\n(Numbers 0-10 for toggling)" +
+				"\n1: Diagonal pathfinding - " + displayInfo(player.allowDiagonals).c_str() +
+				"\n2: OpenList Colors - " + displayInfo(player.showOpenListColor).c_str() +
+				"\n3: FCost Color - " + displayInfo(player.showFCostColor).c_str() +
+				"\n4: Current Path Colors - " + displayInfo(player.showCurrentPathColor).c_str() +
+				"\n5: Path Colors - " + displayInfo(player.showPathColor).c_str() +
+				"\n6: Start Colors - " + displayInfo(player.showStartColor).c_str() +
+				"\n7: Goal Colors - " + displayInfo(player.showGoalColor).c_str() +
+				"\n8: Toggle Tiles Not Checked Colors - " + displayInfo(player.showTilesNotCheckedColor).c_str() +
+				"\n9: Reset all colors off - " + displayMsg(player.isAllOff(), "True", "False") +
+				"\n0: Reset all colors on - " + displayMsg(!player.isAllOff(), "True", "False") +
+				"\nR key: Toggle Mode: " + displayMsg(player.isRealTimeMode,"Real-Time","Step-Time") + " pathfinding";
+			directions.setPosition(4, 0);
+			directions.setString(str);
+		}
+
+
 		/* draw text for some direction */
 		window.draw(directions);
 
 		window.display();
 	}
+}
+
+/* can use to allow the enemies to only attack within a specific distance*/
+float distanceBetweenPoints(sf::Vector2f position1, sf::Vector2f position2)
+{
+	return (abs(sqrt(((position1.x - position2.x) * (position1.x - position2.x)) + ((position1.y - position2.y) * (position1.y - position2.y)))));
+
+}
+
+// for visual information 
+std::string displayInfo(bool status)
+{
+	return status ? std::string("On") : std::string("Off");
+}
+// for visual information 
+std::string displayMsg(bool status, const std::string& isTrue, const std::string& isFalse)
+{
+	return status ? isTrue : isFalse;
 }
